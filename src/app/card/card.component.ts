d822c168651373351c078dacaf5d08465ccad174
UMOR YE <matej.pesjak@adacta-fintech.com>
2020-10-24 23:44:42 -0500

    Added cursor

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -9,14 +9,23 @@ export class CardComponent {
   @Input() offer: Offer;
   changeText = false;
   index: number;
+  hoverAll: boolean;
+  hoverText: boolean;
 
   constructor() {
     this.index = 0;
   }
 
-  changeImage() {
-    this.index = (this.index + 1) % this.offer.images.length;
-    this.offer.url = this.offer.images[this.index]["src"];
+  changeImage(value: number) {
+    let newValue = this.index + value;
+
+    if (newValue < 0) {
+      this.index = this.offer.images.length - 1;
+      this.offer.url = this.offer.images[this.index]["src"];
+    } else {
+      this.index = (this.index + value) % this.offer.images.length;
+      this.offer.url = this.offer.images[this.index]["src"];
+    }
   }
 }
 
